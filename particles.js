d6a584ff8be8c8aba6ce158a46dbf052c09aacb4
Jan Quintus <jan.quintus@dat.de>
2019-08-09 00:18:01 +0200

    ie support

--- particles.js
+++ particles.js
@@ -1,3 +1,16 @@
+// IE Fix
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, "includes", {
+        enumerable: false,
+        value: function(obj) {
+            var newArr = this.filter(function(el) {
+            return el == obj;
+            });
+            return newArr.length > 0;
+        }
+    });
+    }
+
 window.particles = {
     canvas: null,
     ctx: null,
@@ -15,27 +28,27 @@ window.particles = {
         window.particles.height = document.getElementById('particles').offsetHeight;
     
         window.particles.initParticles();
-        if(window.scrollY < window.particles.height) window.particles.start();
+        if(window.particles.isInViewport(document.getElementById('particles'))) window.particles.start();
         
         window.addEventListener('resize', function() {
             window.particles.width = document.getElementById('particles').offsetWidth;
             window.particles.height = document.getElementById('particles').offsetHeight;
             window.particles.initParticles();
-            if(window.scrollY < window.particles.height) window.particles.start();
+            if(window.particles.isInViewport(document.getElementById('particles'))) window.particles.start();
         });
     
         window.addEventListener("scroll", function() {
-            if(window.scrollY > window.particles.height && window.particles.running){
+            if(!window.particles.isInViewport(document.getElementById('particles')) && window.particles.running){
                 window.particles.stop();
             }
-            if(window.scrollY < window.particles.height && !window.particles.running){
+            if(window.particles.isInViewport(document.getElementById('particles')) && !window.particles.running){
                 window.particles.start();
             }
         });
     },
 
     initParticles: function() {
-        let html = '<canvas id="particles-canvas" width="' + window.particles.width + '" height="' + window.particles.height + '"></canvas>'
+        var html = '<canvas id="particles-canvas" width="' + window.particles.width + '" height="' + window.particles.height + '"></canvas>'
         
         document.getElementById('particles').style.backgroundColor = 'rgb(' + window.particles.settings.backgroundColor.r +
                                                     ', ' + window.particles.settings.backgroundColor.g + 
@@ -111,7 +124,9 @@ window.particles = {
     },
 
     updateParticles: function() {
-        for(var particle of window.particles.particles){
+        for(var i = 0; i < window.particles.particles.length; i++){
+            var particle = window.particles.particles[i];
+
             particle.x += particle.velocityX;
             particle.y += particle.velocityY;
             particle.z += particle.velocityZ * particle.zVelocityBoost;
@@ -119,7 +134,7 @@ window.particles = {
             if(particle.zVelocityBoost > 1) particle.zVelocityBoost -= (window.particles.settings.z * 0.04) / 20;
             if(particle.zVelocityBoost < 1) particle.zVelocityBoost = 1;
 
-            let size = ((particle.z / window.particles.settings.z) * window.particles.settings.maxSize).toFixed(2) / 2;
+            var size = ((particle.z / window.particles.settings.z) * window.particles.settings.maxSize).toFixed(2) / 2;
     
             if(particle.x > window.particles.canvas.width + size){
                 particle.x = -size;
@@ -149,7 +164,7 @@ window.particles = {
             }
         }
     
-        window.particles.particles.sort((a, b) => {
+        window.particles.particles.sort(function(a, b) {
             return a.z - b.z;
         });
     
@@ -160,20 +175,24 @@ window.particles = {
         window.particles.ctx.clearRect(0, 0, window.particles.canvas.width, window.particles.canvas.height);
         window.particles.ctx.lineWidth  = window.particles.settings.lineThikness;
     
-        for(var particle of window.particles.particles){
-            let opacity = (particle.z / window.particles.settings.z).toFixed(5);
+        for(var i = 0; i < window.particles.particles.length; i++){
+            var particle = window.particles.particles[i];
+
+            var opacity = (particle.z / window.particles.settings.z).toFixed(5);
     
             if(window.particles.settings.fadeOut){
-                let nXDist = 1 - (Math.abs(particle.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
-                let nYDist = 1 - (Math.abs(particle.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
+                var nXDist = 1 - (Math.abs(particle.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
+                var nYDist = 1 - (Math.abs(particle.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
                 opacity *= nXDist;
                 opacity *= nYDist;
             }
     
-            let size = (window.particles.settings.fadeOutAffectsSize ? opacity : (particle.z / window.particles.settings.z).toFixed(5)) * window.particles.settings.maxSize;
+            var size = (window.particles.settings.fadeOutAffectsSize ? opacity : (particle.z / window.particles.settings.z).toFixed(5)) * window.particles.settings.maxSize;
     
             particle.connected = [];
-            for(var other of window.particles.particles){
+            for(var j = 0; j < window.particles.particles.length; j++){
+                var other = window.particles.particles[j];
+
                 if(other != particle && !other.connected.includes(particle)){
                     if(other.x < particle.x + window.particles.settings.connectionBox && other.x > particle.x - window.particles.settings.connectionBox
                         && other.y < particle.y + window.particles.settings.connectionBox && other.y > particle.y - window.particles.settings.connectionBox
@@ -181,17 +200,17 @@ window.particles = {
     
                         particle.connected.push(other);
                         
-                        let otherOpacity = (other.z / window.particles.settings.z).toFixed(5);
+                        var otherOpacity = (other.z / window.particles.settings.z).toFixed(5);
                         if(window.particles.settings.fadeOut){
-                            let nXDist = 1 - (Math.abs(other.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
-                            let nYDist = 1 - (Math.abs(other.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
+                            var nXDist = 1 - (Math.abs(other.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
+                            var nYDist = 1 - (Math.abs(other.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
                             otherOpacity *= nXDist;
                             otherOpacity *= nYDist;
                         }
     
-                        let gradient = window.particles.ctx.createLinearGradient(particle.x, particle.y, other.x, other.y);
-                        gradient.addColorStop(0, window.particles.getBlendedColor(opacity));
-                        gradient.addColorStop(1, window.particles.getBlendedColor(otherOpacity));
+                        var gradient = window.particles.ctx.createLinearGradient(particle.x, particle.y, other.x, other.y);
+                        gradient.addColorStop("0", window.particles.getBlendedColor(opacity));
+                        gradient.addColorStop("1", window.particles.getBlendedColor(otherOpacity));
                         window.particles.ctx.strokeStyle = gradient;
                         window.particles.ctx.beginPath();
                         window.particles.ctx.moveTo(particle.x, particle.y);
@@ -215,10 +234,10 @@ window.particles = {
             window.particles.ctx.fill();
     
             if(particle.plong.lifetime > 0){
-                let opacity = (particle.plong.lifetime / window.particles.settings.plongLifeTime).toFixed(2);
+                var opacity = (particle.plong.lifetime / window.particles.settings.plongLifeTime).toFixed(2);
                 if(window.particles.settings.fadeOut){
-                    let nXDist = 1 - (Math.abs(particle.plong.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
-                    let nYDist = 1 - (Math.abs(particle.plong.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
+                    var nXDist = 1 - (Math.abs(particle.plong.x - (window.particles.width / 2)) / (window.particles.width / 2)) * window.particles.settings.fadeOutStrength;
+                    var nYDist = 1 - (Math.abs(particle.plong.y - (window.particles.height / 2)) / (window.particles.height / 2)) * window.particles.settings.fadeOutStrength;
                     opacity *= nXDist;
                     opacity *= nYDist;
                 }
@@ -227,7 +246,7 @@ window.particles = {
                                     ', ' + window.particles.settings.particleColor.b + 
                                     ',' + opacity + ')';
 
-                let size = (1 - ((particle.plong.lifetime * .8) / window.particles.settings.plongLifeTime)) * (window.particles.settings.plongSize);
+                var size = (1 - ((particle.plong.lifetime * .8) / window.particles.settings.plongLifeTime)) * (window.particles.settings.plongSize);
                 window.particles.ctx.beginPath();
                 window.particles.ctx.arc(particle.plong.x, particle.plong.y, size * Math.cos(opacity), 0, 2 * Math.PI);
                 window.particles.ctx.arc(particle.plong.x, particle.plong.y, size * Math.cos(opacity) * Math.sin(opacity) * 2, 0, 2 * Math.PI);
@@ -240,10 +259,35 @@ window.particles = {
 
     getBlendedColor: function(opacity) { 
 
-        let r = (window.particles.settings.backgroundColor.r * (1 - opacity)) + ((window.particles.settings.particleColor.r + window.particles.settings.particleColorBoost) * (opacity));
-        let g = (window.particles.settings.backgroundColor.g * (1 - opacity)) + ((window.particles.settings.particleColor.g + window.particles.settings.particleColorBoost) * (opacity));
-        let b = (window.particles.settings.backgroundColor.b * (1 - opacity)) + ((window.particles.settings.particleColor.b + window.particles.settings.particleColorBoost) * (opacity));
+        var r = (window.particles.settings.backgroundColor.r * (1 - opacity)) + ((window.particles.settings.particleColor.r + window.particles.settings.particleColorBoost) * (opacity));
+        var g = (window.particles.settings.backgroundColor.g * (1 - opacity)) + ((window.particles.settings.particleColor.g + window.particles.settings.particleColorBoost) * (opacity));
+        var b = (window.particles.settings.backgroundColor.b * (1 - opacity)) + ((window.particles.settings.particleColor.b + window.particles.settings.particleColorBoost) * (opacity));
 
-        return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+        if(r > 255) r = 255;
+        if(g > 255) g = 255;
+        if(b > 255) b = 255;
+        if(r < 0) r = 0;
+        if(g < 0) g = 0;
+        if(b < 0) b = 0;
+
+        var rHex = Number(Math.floor(r)).toString(16);
+        if (rHex.length < 2) rHex = "0" + rHex;
+        var gHex = Number(Math.floor(g)).toString(16);
+        if (gHex.length < 2) gHex = "0" + gHex;
+        var bHex = Number(Math.floor(b)).toString(16);
+        if (bHex.length < 2) bHex = "0" + bHex;
+        
+        return '#' + rHex + gHex + bHex;
+    },
+
+    isInViewport: function (elem) {
+        var bounding = elem.getBoundingClientRect();
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
+    
 }
